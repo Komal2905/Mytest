fa49c86878f3c021b4f227406b8557bfee86ebee
komal2905 <komal2905@gmail.com>
2015-12-01 12:05:51 +0530

    FullName

---
+++ FUllName.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(){
+    
+    NSString *fName=@"komal";
+    NSString *lName=@"Kamble";
+    NSLog(@"My full name is : %@ %@", fName,lName);
+    
+    return 0;
+}
